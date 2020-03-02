85b81535876bee4c2ae8f8feb59ae575fbf17d69
Hudson Chamberlain <hudson.m.chamberlain@gmail.com>
2020-03-02 12:19:56 -0600

    begin cors

--- api/server.js
+++ api/server.js
@@ -7,12 +7,27 @@ const authenticate = require('../auth/authenticate-middleware.js');
 
 const server = express();
 
+var whitelist = ['http://localhost:5000'] // todo add frontend hosted
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
 
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
 
-server.use('/api/auth', authRouter);
+server.use('/api/auth', cors(corsOptions), authRouter);
 // server.use('/api/dev', authenticate, devRouter);
+
+server.get('/', (req, res) => {
+    res.sta
+})
 
 module.exports = server;
