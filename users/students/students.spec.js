199bd189fe288d83d7cdf68621b4f183e186fc4c
viscountfam <V3ctor42!>
2020-03-04 20:18:42 -0500

    fixing a merge conflict caused by the test db and adding some tests

--- users/students/students.spec.js
+++ users/students/students.spec.js
@@ -3,8 +3,20 @@ const db = require('../../data/dbConfig.js')
 const student
 const server = require('../../api/server.js')
 
-describe('testing the database methods of the users table', () => {
+describe('testing the add function on the database', () => {
     beforeEach(async () => {
-        await db('')
-    })
-})
+        await db('users').truncate();
+    });
+
+    describe('add function', () => {
+        it('add new users into the db', async () => {
+            let userNumber;
+            userNumber = await db('users'
+            await auth.add({ username: 'Harry', password: 'potter', userType: '0'})
+            await auth.add({ username: 'Ron', password: 'weasley', userType: '1'})
+            await auth.add({ username: 'Hermoine', password: 'granger', userType: '0'})
+            userNumber = await db('users');
+            expect(userNumber).toHaveLength(3)
+        });
+    });
+});
